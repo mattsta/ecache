4f26b523a90b3c3aa58edc0ac3278893e58c5e00
Led <ledest@gmail.com>
2018-02-07 14:08:42 +0200

    ecache_reaper: remade for use gen_server's timer

--- src/ecache_reaper.erl
+++ src/ecache_reaper.erl
@@ -7,8 +7,8 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--export([ecache_reaper/2]). % quiet unused function annoyance
--record(reaper, {cache_size}).
+-record(reaper, {name, size}).
+-define(TIMEOUT, 4000).
 
 start_link(Name) -> start_link(Name, 8).
 
@@ -20,32 +20,22 @@ start(Name, Size) -> gen_server:start(?MODULE, [Name, Size], []).
 %%% Callback functions from gen_server
 %%%----------------------------------------------------------------------
 
-shrink_cache_to_size(_Name, CurrentSize, Size) when CurrentSize =< Size -> ok;
-shrink_cache_to_size(Name, _CurrentSize, Size) ->
-    gen_server:call(Name, reap_oldest),
-    shrink_cache_to_size(Name, ecache:total_size(Name), Size).
+shrink_cache_to_size(Name, CurrentSize, Size) ->
+    CurrentSize > Size andalso shrink_cache_to_size(Name, gen_server:call(Name, reap_oldest), Size).
 
-ecache_reaper(Name, Size) ->
-    % sleep for 4 seconds
-    shrink_cache_to_size(Name, ecache:total_size(Name), Size),
-    timer:apply_after(4000, ?MODULE, ecache_reaper, [Name, Size]).
+init([Name, Size]) -> {ok, #reaper{name = Name, size = Size}, ?TIMEOUT}.
 
-init([_Name, SizeBytes] = Args) ->
-    % ecache_reaper is started from ecache_server, but ecache_server can't finish
-    % init'ing % until ecache_reaper:init/1 returns.
-    % Use apply_after to make sure ecache_server exists when making calls.
-    % Don't be clever and take this timer away.  Compensates for chicken/egg prob.
-    timer:apply_after(4000, ?MODULE, ecache_reaper, Args),
-    {ok, #reaper{cache_size = SizeBytes}}.
+handle_call(Arbitrary, _From, State) -> {reply, {arbitrary, Arbitrary}, State, ?TIMEOUT}.
 
-handle_call(Arbitrary, _From, State) -> {reply, {arbitrary, Arbitrary}, State}.
-
-handle_cast(_Request, State) -> {noreply, State}.
+handle_cast(_Request, State) -> {noreply, State, ?TIMEOUT}.
 
 terminate(_Reason, _State) -> ok.
 
+handle_info(timeout, #reaper{name = Name, size = Size} = State) ->
+    shrink_cache_to_size(Name, ecache:total_size(Name), Size),
+    {noreply, State, ?TIMEOUT};
 handle_info(Info, State) ->
     error_logger:warning_msg("Other info of: ~p~n", [Info]),
-    {noreply, State}.
+    {noreply, State, ?TIMEOUT}.
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
